c516f8855cc078e560c2286ce0bbed827be45991
Ubuntu <ubuntu@ip-172-31-24-79.ec2.internal>
2021-11-08 12:32:35 +0000

    8th

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -34,7 +34,7 @@ catch(NumberFormatException ex)
 return;
 }
 
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C',D','E','F'};
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
         String hexadecimal="";
