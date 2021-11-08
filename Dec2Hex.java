1526036a64f6d466f6ec22acdf2c2c05aadc5301
Ubuntu <ubuntu@ip-172-31-24-79.ec2.internal>
2021-11-08 12:25:11 +0000

    6th

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -34,43 +34,11 @@ catch(NumberFormatException ex)
 return;
 }
 
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','$
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C',D','E','F'};
         int rem, num;
         num = Arg1;
         String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + "$
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-
-        System.out.println("Hexadecimal representation is: " + hexad$
-
-    }
-}
-
-
-
-
-
-                         import java.util.Scanner;
-
-class Dec2Hex
-{
-
-    public static int Arg1;
-
-    public static void main(String args[])
-    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        System.out.println("Converting the Decimal Value " + num + "to Hex...");
 
         while(num != 0)
         {
@@ -82,19 +50,5 @@ class Dec2Hex
         System.out.println("Hexadecimal representation is: " + hexadecimal);
 
     }
-public static int validation(){
-
-int ip = 0;
-boolean tf=true;
-scanner scan = new Scanner(System.in);
-
-do {
-System.outPrintln("Please enter an integer, in order to convert it into a hexidecimal!");
-
-
-
-
-}
-
 }
 
